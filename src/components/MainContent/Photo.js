8444321d801916dd88d8fcf30ba2df834a177448
unknown <ken.brandon@aol.com>
2020-02-06 14:53:16 -0700

    MVP

--- src/components/MainContent/Photo.js
+++ src/components/MainContent/Photo.js
@@ -1,15 +1,12 @@
 import React from "react";
-import styled from 'styled-components';
+// import styled from "styled-components";
 
-
-export default function Photo (props) {
+export default function Photo(props) {
   return (
     <div className="Photo" key={props.image.date}>
       <h2>{props.image.title}</h2>
-      <img
-        className="imgPhoto"
-        src={props.image.url} alt={props.image.title}/>
+      <img className="imgPhoto" src={props.image.url} alt={props.image.title} />
       <p>{props.image.explanation}</p>
     </div>
-    );
+  );
 }
--- src/components/TheHeader/Head.js
+++ src/components/TheHeader/Head.js
@@ -1,45 +1,53 @@
-import React from 'react';
-import Photo from '../MainContent/Photo';
-import styled from 'styled-components';
+import React from "react";
+import Photo from "../MainContent/Photo";
+import styled from "styled-components";
 
 const Button = styled.button`
-height: 30px;
-width: 10%;
-border-radius: 30px;
-margin: 0 50px;
-background: white;
+  height: 30px;
+  width: 10%;
+  border-radius: 30px;
+  margin: 0 50px;
+  background: white;
 `;
 const Input = styled.input`
-border-radius:30px;
-padding-left: 35px;
+  border-radius: 30px;
+  padding-left: 35px;
 `;
 
-export default function Header (props) {
+export default function Header(props) {
   return (
     <div className="Header">
       <div className="buttons">
-
         <Button
-        className="button"
-        onClick={() =>{
-          return props.func("left")  }}> Previous Day
+          className="button"
+          onClick={() => {
+            return props.func("left");
+          }}
+        >
+          {" "}
+          Previous Day
         </Button>
 
         <Input
-        id="dateChange"
-         type="date"
-         value={props.date}
-         onChange={(event) => {return props.func(event.target.value);
-        }} />
+          id="dateChange"
+          type="date"
+          value={props.date}
+          onChange={event => {
+            return props.func(event.target.value);
+          }}
+        />
 
-        <Button className="button" onClick={() =>{
-          return props.func("right")
-        }}>Next Day
+        <Button
+          className="button"
+          onClick={() => {
+            return props.func("right");
+          }}
+        >
+          Next Day
         </Button>
       </div>
 
-        <Photo
-        image={props.image}/>
-        </div>
-    )
+      <Photo image={props.image} />
+    </div>
+  );
 }
